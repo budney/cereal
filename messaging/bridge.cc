b0bfcbb1ea79a0585998981204a961dbddea5850
Len Budney <Len.Budney@grantstreet.com>
2022-06-07 14:04:15 -0400

    Revert to commit "51f7e00af4030f2b3a8d60e2038db87a0cd5bced", which was used by budney-torque prior to the disastrous attempt to merge subaru-community-2022-06-03.

--- messaging/bridge.cc
+++ messaging/bridge.cc
@@ -11,11 +11,6 @@ typedef void (*sighandler_t)(int sig);
 #include "impl_zmq.h"
 #include "services.h"
 
-std::atomic<bool> do_exit = false;
-static void set_do_exit(int sig) {
-  do_exit = true;
-}
-
 void sigpipe_handler(int sig) {
   assert(sig == SIGPIPE);
   std::cout << "SIGPIPE received" << std::endl;
@@ -36,8 +31,6 @@ static std::vector<std::string> get_services(std::string whitelist_str, bool zmq
 
 int main(int argc, char** argv) {
   signal(SIGPIPE, (sighandler_t)sigpipe_handler);
-  signal(SIGINT, (sighandler_t)set_do_exit);
-  signal(SIGTERM, (sighandler_t)set_do_exit);
 
   bool zmq_to_msgq = argc > 2;
   std::string ip = zmq_to_msgq ? argv[1] : "127.0.0.1";
@@ -74,13 +67,11 @@ int main(int argc, char** argv) {
     sub2pub[sub_sock] = pub_sock;
   }
 
-  while (!do_exit) {
+  while (true) {
     for (auto sub_sock : poller->poll(100)) {
       Message * msg = sub_sock->receive();
       if (msg == NULL) continue;
-      int ret;
-      do { ret = sub2pub[sub_sock]->sendMessage(msg); } while (ret == -1 && errno == EINTR);
-      assert(ret >= 0);
+      sub2pub[sub_sock]->sendMessage(msg);
       delete msg;
     }
   }
